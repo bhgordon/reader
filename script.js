45cf51998f065d23194d3ed90de6cd3a3f6fe098
Ben Gordon <benharrisgordon@gmail.com>
2022-02-06 10:53:21 +0100

    Added font selector

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+const body = document.querySelector("body");
 const scrollProgress = document.getElementById('scroll-progress');
 const height = document.documentElement.scrollHeight;
 const percentage = document.querySelector(".percentage");
+const fontSelection = document.querySelectorAll(".font-selection");
+const fontSelector = document.querySelector(".font-selector");
+const fontBtn = document.querySelector(".font-selector-btn");
+console.log(fontSelector);
 
 window.addEventListener('scroll', () => {
   const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
@@ -8,3 +13,15 @@ window.addEventListener('scroll', () => {
   const progress = Math.floor(parseInt(`${(scrollTop / height) * 100}%`), 10);
   percentage.innerText = `${progress}%`;
 });
+
+fontSelection.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const font = item.id;
+    body.style.fontFamily = font;
+  })
+})
+
+fontBtn.addEventListener('click', (e) => {
+  console.log("clicked");
+  fontSelector.classList.toggle("visible");
+})
